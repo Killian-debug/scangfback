47641147db9868b6f5f4be831eed2ee2a334e544
Killian-debug <kitsunkilli@gmail.com>
2022-11-24 14:34:05 +0100

    last

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ let server = app.listen(port, () => {
 });
 
 app.get('/', (req, res) => {
-    res.send('Hey this is my API running 🥳')
+    res.send('Hey this is my API running ' + port + '' + process.env.DB_NAME + '' + process.env.DB_PASSWORD )
   })
 
